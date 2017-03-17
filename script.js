c7aff78b61172a450cc0da04b7734f10437fa98b
Lenilson Nascimento <lenilson.100@gmail.com>
2017-03-17 18:00:41 -0300

    Creating animation events

--- script.js
+++ script.js
@@ -11,6 +11,14 @@
 
       content.classList.add('is-active');
 
+      var front = content.querySelector('.front');
+      var back = content.querySelector('.back');
+
+      setTimeout(function() {
+        front.classList.add('front-flipped');
+        back.classList.add('back-flipped');
+      }, 500)
+
       var button = content.querySelector('button');
       button.addEventListener('click', function(event) {
         event.preventDefault();
